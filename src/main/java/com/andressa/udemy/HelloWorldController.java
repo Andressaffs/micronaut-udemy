051ad15d02e82ded38a24d3d775a9812100fca24
Andressa Ferreira <andressaffss@hotmail.com>
2021-07-01 11:48:26 -0300

    first commit

---
+++ src/main/java/com/andressa/udemy/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.andressa.udemy;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/hello")
+public class HelloWorldController {
+
+    @Get("/")
+    public String index(){
+        return "Hello World";
+    }
+}
