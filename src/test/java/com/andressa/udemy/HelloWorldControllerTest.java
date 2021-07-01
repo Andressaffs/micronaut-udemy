2547d1055cbebf08fec452862893c8ef5aa8ae6b
Andressa Ferreira <andressaffss@hotmail.com>
2021-07-01 12:10:58 -0300

    junit: create unit test with micronaut

--- src/test/java/com/andressa/udemy/HelloWorldControllerTest.java
+++ src/test/java/com/andressa/udemy/HelloWorldControllerTest.java
@@ -1,5 +1,7 @@
 package com.andressa.udemy;
 
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
@@ -7,15 +9,26 @@ import org.junit.jupiter.api.Assertions;
 
 import javax.inject.Inject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @MicronautTest
-class MnStockBrokerTest {
+class HelloWorldControllerTest {
 
     @Inject
     EmbeddedApplication<?> application;
+
+    @Inject
+    @Client ("/")
+    RxHttpClient client;
 
     @Test
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
     }
 
+    @Test
+    void testHelloResponse() {
+        final String result = client.toBlocking().retrieve("/hello");
+        assertEquals("Hello World", result);
+    }
 }
